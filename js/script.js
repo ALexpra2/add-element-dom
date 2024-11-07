690dd3f468d5ec10906fd5086323468364ae6555
Alexpra2 <alexpra2@gmail.com>
2024-11-08 00:01:29 +0100

    Solution

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+const boton = document.getElementById("agregar");
+const lista = document.getElementById("lista");
+
+
+boton.addEventListener("click", function(){
+    const datos=prompt("Introduce un elemento para la lista")
+    const elementList = document.createElement ("li");
+    elementList.textContent = datos;
+    lista.appendChild(elementList);
+})
+

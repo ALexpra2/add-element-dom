ebb68bde4021540d18b2585169731783fcad99f4
Alexpra2 <alexpra2@gmail.com>
2024-11-18 23:11:36 +0100

    addspace

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ boton.addEventListener("click", function(){
     const datos=prompt("Introduce un elemento para la lista")
     const elementList = document.createElement ("li");
     elementList.textContent = datos;
-    lista.appendChild(elementList); 
+    lista.appendChild(elementList);
+     
 })
 
